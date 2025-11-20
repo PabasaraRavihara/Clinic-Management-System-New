8310a182e67664e4b67573a75626c866adc07420
imidu <hansana-ps21085@stu.kln.ac.lk>
2025-11-20 09:41:45 +0530

    Delete AppointmentRepositary.java

--- AppointmentRepositary.java
+++
@@ -1,9 +0,0 @@
-package com.example.Clinic_Management_System.repository;
-import com.example.Clinic_Management_System.model.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface AppointmentRepositary extends JpaRepository<Appointment, Long>{
-    List<Appointment> findAllById(Long doctorId);
-}
