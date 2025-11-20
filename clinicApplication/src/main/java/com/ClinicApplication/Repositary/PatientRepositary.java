90d26b9594dc1434d4a39b9840fe64d81539ca11
imidu <hansana-ps21085@stu.kln.ac.lk>
2025-11-20 09:26:48 +0530

    Delete clinicApplication directory

--- clinicApplication/src/main/java/com/ClinicApplication/Repositary/PatientRepositary.java
+++
@@ -1,6 +0,0 @@
-package com.ClinicApplication.Repositary;
-import com.ClinicApplication.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepositary extends JpaRepository<Patient, Long>{
-}
